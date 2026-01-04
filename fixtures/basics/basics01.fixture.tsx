fc61081f9e437e8be2f0fa96b8de99574c73153e
seveibar <seveibar@gmail.com>
2026-01-04 12:01:11 -0800

    some cosmos setup

---
+++ fixtures/basics/basics01.fixture.tsx
@@ -0,0 +1,9 @@
+import { GenericSolverDebugger } from "@tscircuit/solver-utils/react"
+import { JumperGraphSolver } from "../../lib/JumperGraphSolver/JumperGraphSolver"
+import inputProblem from "./basics01-input.json"
+
+export default () => (
+  <GenericSolverDebugger
+    createSolver={() => new JumperGraphSolver(inputProblem)}
+  />
+)
